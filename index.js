cb21419ad0c860d783fb655ebd616cdf6315bc78
marshall chen <marshallchan666@outlook.com>
2021-07-10 23:56:35 +0800

    first d3

---
+++ index.js
@@ -0,0 +1,9 @@
+const canvas = d3.select('.canvas');
+
+const svg = canvas.append('svg'); // get a new tag element inside the selector component
+
+// append shapes to svg container
+
+svg.append('rect');
+svg.append('circle');
+svg.append('line');
